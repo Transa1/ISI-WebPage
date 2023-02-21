7d54b43eff038e8642a3c39f8d56005664715dae
stephaniedlp <97551347+stephaniedlp@users.noreply.github.com>
2023-02-20 21:10:10 -0700

    se anadio prototipo de navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
 import { SharedModule } from './shared/shared.module';
