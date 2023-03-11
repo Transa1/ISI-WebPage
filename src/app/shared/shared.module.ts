d6af447bcee10681a1a1dd5f88f660004c2881a5
Luis Romo <luisrp9812@gmail.com>
2023-03-11 14:08:49 -0700

    añadido primera version de diseño responisvo en página principal

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ThumbnailComponent } from './thumbnail/thumbnail.component';
 
 @NgModule({
-  declarations: [
-    FooterComponent,
-    HeaderComponent,
-    NavbarComponent,
-    ThumbnailComponent,
-  ],
+  declarations: [FooterComponent, NavbarComponent, ThumbnailComponent],
   imports: [CommonModule],
-  exports: [
-    HeaderComponent,
-    FooterComponent,
-    NavbarComponent,
-    ThumbnailComponent,
-  ],
+  exports: [FooterComponent, NavbarComponent, ThumbnailComponent],
 })
 export class SharedModule {}
