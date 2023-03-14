f10c52ccb59fb36325beade97b1547eebd56966a
andresmc98 <52982047+andresmc98@users.noreply.github.com>
2023-03-14 09:58:49 -0700

    Revert "Merge pull request #24 from ISI-Unison/6-navbar"
    This reverts commit 6a0d0286803153d0f172126db90cd4e0ff7721a9, reversing
    changes made to 1c60c9e7aa64759a53d1f66dbf70fbc9fa0bffca.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
 import { SharedModule } from './shared/shared.module';
-import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
-  declarations: [AppComponent, TimelineComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, PagesModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
