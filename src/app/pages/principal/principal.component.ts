3ef5945ffddf2e1d6c7683f8f0c22747b7c80d43
Luis Romo <luisrp9812@gmail.com>
2023-03-14 09:38:12 -0700

    refactorización página principal

--- src/app/pages/principal/principal.component.ts
+++ src/app/pages/principal/principal.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { tap } from 'rxjs';
+import { distinctUntilChanged, tap } from 'rxjs';
 
 @Component({
   selector: 'app-principal',
@@ -12,38 +12,36 @@ export class PrincipalComponent implements OnInit {
   currentBreakpoint: string = '';
 
   readonly breakpoint$ = this.breakpointObserver
-    .observe([Breakpoints.XSmall, Breakpoints.Medium, Breakpoints.Large])
-    .pipe(tap((value) => console.log(value)));
+    .observe([
+      Breakpoints.XSmall,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge,
+    ])
+    .pipe(
+      tap((value) => console.log(value)),
+      distinctUntilChanged()
+    );
 
   constructor(public breakpointObserver: BreakpointObserver) {}
   ngOnInit(): void {
     this.breakpoint$.subscribe(() => this.breakpointChanged());
   }
   private breakpointChanged() {
-    if (this.breakpointObserver.isMatched(Breakpoints.Large)) {
-      this.currentBreakpoint = Breakpoints.Large;
-      console.log('large');
+    if (this.breakpointObserver.isMatched(Breakpoints.XSmall)) {
+      this.currentBreakpoint = Breakpoints.XSmall;
+      console.log('sm');
     }
     if (this.breakpointObserver.isMatched(Breakpoints.Medium)) {
       this.currentBreakpoint = Breakpoints.Medium;
       console.log('med');
     }
-    if (this.breakpointObserver.isMatched(Breakpoints.XSmall)) {
-      this.currentBreakpoint = Breakpoints.XSmall;
-      console.log('sm');
+    if (
+      this.breakpointObserver.isMatched(Breakpoints.Large) ||
+      this.breakpointObserver.isMatched(Breakpoints.XLarge)
+    ) {
+      this.currentBreakpoint = Breakpoints.Large;
+      console.log('large');
     }
   }
-  // this.responsive
-  //   .observe([Breakpoints.XSmall, Breakpoints.Medium, Breakpoints.Large])
-  //   .subscribe((result) => {
-  //     const breakpoints = result.breakpoints;
-  //     if (breakpoints[Breakpoints.XSmall]) {
-  //       this.currentBreakpoint = Breakpoints.XSmall;
-  //       console.log('MAMAWBO small');
-  //     } else if (breakpoints[Breakpoints.Medium]) {
-  //       console.log('MAMAWBO medium');
-  //     } else if (breakpoints[Breakpoints.Large]) {
-  //       console.log('MAMAWBO large');
-  //     }
-  //   });
 }
