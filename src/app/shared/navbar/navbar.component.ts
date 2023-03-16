9bbf3a8525de214b9fbf29ae214b8965a914bec4
stephaniedlp <97551347+stephaniedlp@users.noreply.github.com>
2023-03-16 13:29:42 -0700

    navbar creo

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { distinctUntilChanged, tap } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +9,7 @@ import { Component, HostListener } from '@angular/core';
 })
 
 
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
   
   printMsg() {
     console.log('Icon Clicked');
@@ -17,11 +19,48 @@ export class NavbarComponent {
   @HostListener('window:scroll', [])
   onWindowScroll = () => {
     console.log(document.documentElement.scrollTop);
-
+    
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
      this.isNavbarScrolled=true;
     } else {
       this.isNavbarScrolled= false;
+    }
+
+  }
+  Breakpoints = Breakpoints;
+  currentBreakpoint: string = '';
+
+  readonly breakpoint$ = this.breakpointObserver
+    .observe([
+      Breakpoints.XSmall,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge,
+    ])
+    .pipe(
+      tap((value) => console.log(value)),
+      distinctUntilChanged()
+    );
+
+  constructor(public breakpointObserver: BreakpointObserver) {}
+  ngOnInit(): void {
+    this.breakpoint$.subscribe(() => this.breakpointChanged());
+  }
+  private breakpointChanged() {
+    if (this.breakpointObserver.isMatched(Breakpoints.XSmall)) {
+      this.currentBreakpoint = Breakpoints.XSmall;
+      console.log('sm');
+    }
+    if (this.breakpointObserver.isMatched(Breakpoints.Medium)) {
+      this.currentBreakpoint = Breakpoints.Medium;
+      console.log('med');
+    }
+    if (
+      this.breakpointObserver.isMatched(Breakpoints.Large) ||
+      this.breakpointObserver.isMatched(Breakpoints.XLarge)
+    ) {
+      this.currentBreakpoint = Breakpoints.Large;
+      console.log('large');
     }
   }
 }
